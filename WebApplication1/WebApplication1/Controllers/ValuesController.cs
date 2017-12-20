0628bff17a16dd495dc9f2e72a5f8e53da7f3293
Sura <sura@gmail.com>
2017-12-21 02:56:41 +0530

    m

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Controllers
 		// comment by suranga
         // GET api/values
 		//  comment in master branch
-        public IEnumerable<string> Get()
+        public IEnumerable<string> Get()//mast
         {
             return new string[] { "value1", "value2" };
         }
